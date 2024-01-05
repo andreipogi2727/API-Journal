75f820a36892b999fc458051a745c1b812ae862f
Andrei Haboc <Ahaboc3536@conestogac.on.ca>
2024-01-04 22:51:34 -0500

    Set up express server and connect to mongoDB

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-//initial
-//second test commit
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+const app = express()
+
+mongoose
+.connect('mongodb+srv://admin:admin123@devandreiapi.ohinqtp.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() =>
+{
+app.listen(3000, ()=>
+{
+    console.log('Journal API app is running on port 3000')
+})
+    console.log('Succesfully connected to MongoDB')
+}).catch((error)=>
+{
+    console.log(error)
+})
+
+
