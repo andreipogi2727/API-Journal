746da6fb296bf075ed9eba48f859066930b18e78
Andrei Haboc <Ahaboc3536@conestogac.on.ca>
2024-01-05 03:10:29 -0500

    Making basic Ui for frontend

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 const Journal = require('./models/journalModel')
 const app = express()
 const journalRoute = require('./routes/journalRoute')
-
+const cors = require('cors')
 
 
 app.use(express.json())
+app.use(cors())
 app.use('/journals', journalRoute )
 
 
